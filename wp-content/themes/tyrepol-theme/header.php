f94e1585e247dca7eb542059218815fc061d1ee7
Bogdan Boberski <bogdanboberski@MacBook-Pro-M4-Pszek.local>
2026-08-09 09:31:07 +0200

    Changed header links

--- wp-content/themes/tyrepol-theme/header.php
+++ wp-content/themes/tyrepol-theme/header.php
@@ -7,34 +7,48 @@
   <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg" type="image/svg+xml">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+  
   <?php wp_head(); ?> 
 </head>
 <body <?php body_class(); ?>>
 
   <header class="header">
     <div class="header__inner">
-      <a href="<?php echo home_url(); ?>" class="header__logo">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="header__logo">
         <img class="header__logo-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="TyrePol Logo">
       </a>
 
       <nav class="header__nav" id="header-nav">
         <ul class="header__list">
-          <li class="header__item"><a class="header__link header__link--active" href="<?php echo home_url(); ?>">Główna</a></li>
-          <li class="header__item"><a class="header__link" href="opony.html">Opony</a></li>
-          <li class="header__item"><a class="header__link" href="marki.html">Marki</a></li>
-          <li class="header__item"><a class="header__link" href="saucerman.html">Saucerman</a></li>
-          <li class="header__item"><a class="header__link" href="#">O TyrePol</a></li>
+          <li class="header__item">
+            <a class="header__link <?php echo is_front_page() ? 'header__link--active' : ''; ?>" href="<?php echo esc_url(home_url('/')); ?>">Główna</a>
+          </li>
+          <li class="header__item">
+            <a class="header__link" href="<?php echo esc_url(home_url('/opony/')); ?>">Opony / Marki</a>
+          </li>
+          <li class="header__item">
+            <a class="header__link" href="<?php echo esc_url(home_url('/saucerman/')); ?>">Saucerman</a>
+          </li>
+          <li class="header__item">
+            <a class="header__link" href="<?php echo esc_url(home_url('/o-firmie/')); ?>">O TyrePol</a>
+          </li>
           <li class="header__item header__item--dropdown">
             <a href="#" class="header__link header__dropdown-toggle">
               Baza wiedzy <span class="header__dropdown-arrow">&#9662;</span>
             </a>
             <ul class="header__dropdown-menu">
               <li class="header__dropdown-item"><a class="header__dropdown-link" href="#">Porady</a></li>
-              <li class="header__dropdown-item"><a class="header__dropdown-link" href="#">Aktualności</a></li>
+              <li class="header__dropdown-item">
+                <a class="header__dropdown-link" href="<?php echo esc_url(home_url('/aktualnosci/')); ?>">Aktualności</a>
+              </li>
               <li class="header__dropdown-item"><a class="header__dropdown-link" href="#">Testy opon</a></li>
             </ul>
           </li>
-          <li class="header__item"><a class="header__link" href="kontakt.html">Kontakt</a></li>
+          <li class="header__item">
+            <a class="header__link" href="<?php echo esc_url(home_url('/kontakt/')); ?>">Kontakt</a>
+          </li>
         </ul>
       </nav>
 
